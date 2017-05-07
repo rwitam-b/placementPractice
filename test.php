5a525d8aab922f00469436356f01efa25a168027
Rwitam Bandyopadhyay <mailtorwitam@gmail.com>
2017-05-07 13:10:54 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 //     if (isset($_SESSION['aotemail_username']) and $_SERVER["REQUEST_METHOD"]=="POST" and isset($_POST["q_id"]) and isset($_POST["text"])){
         $file=getcwd().'/testEmail.py';
 	echo $file;
-        $command = escapeshellcmd('python -V');
+        $command = escapeshellcmd('python -V 2>&1');
         $display = shell_exec($command);        
         echo $display;
 //     }
