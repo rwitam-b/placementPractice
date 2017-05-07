1dd6dfa475ad4cb0590b16733d26ce06546a15f3
Rwitam Bandyopadhyay <mailtorwitam@gmail.com>
2017-05-07 13:13:20 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,13 +1,13 @@
 <?php
-//     require 'tryLogin.php';
-//     if (isset($_SESSION['aotemail_username']) and $_SERVER["REQUEST_METHOD"]=="POST" and isset($_POST["q_id"]) and isset($_POST["text"])){
+    require 'tryLogin.php';
+    if (isset($_SESSION['aotemail_username']) and $_SERVER["REQUEST_METHOD"]=="POST" and isset($_POST["q_id"]) and isset($_POST["text"])){
         $file=getcwd().'/testEmail.py';
 	echo $file;
 //         $command = escapeshellcmd('python -V 2>&1');
         $display = shell_exec('python -V 2>&1');        
         echo $display;
-//     }
-//     else{
-//         echo "Not Available";
-//     }
+    }
+    else{
+        echo "Not Available";
+    }
 ?>
